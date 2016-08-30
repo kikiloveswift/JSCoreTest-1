e3474f7aa77d4b5417f19ca22ae3701550a34539
kingleekong <kong@konglee.local>
2016-08-30 23:15:57 +0800

    保存代码 JS直接调用OC

--- JSCoreTest-1/ViewController.m
+++ JSCoreTest-1/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <JavaScriptCore/JavaScriptCore.h>
 
 @interface ViewController ()<UIWebViewDelegate>
 {
@@ -20,7 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"ROOT PAGE";
+    self.title = @"First PAGE";
     [self initUI];
 }
 
@@ -28,9 +29,50 @@
 {
     testWebView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, kWidth, kHeight)];
     testWebView.delegate = self;
-    NSString *urlString = @"http://172.16.205.108:8080/JSCoreTest/index.html";
+    NSString *urlString = @"http://192.168.0.8:8080/JSCoreTest-1/index.html";
     [testWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]]];
     [self.view addSubview:testWebView];
+}
+
+#pragma mark-DidFinishLoad---WebView加载结束
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+   JSContext *context=[webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    JSValue *value = [context evaluateScript:@"sumA(1,2)"];
+    NSInteger result = [value toInt32];
+    __weak typeof(self) weakself = self;
+    context[@"popAction"] = ^(id value){
+        
+//        NSArray *args = [JSContext currentArguments];
+//        for (JSValue *jsVal in args) {
+//            NSLog(@"%@", jsVal);
+//        }
+//        JSValue *this = [JSContext currentThis];
+//        JSContext *currentCotext = [JSContext currentContext];
+//        JSValue *value = currentCotext[@"popAction"];
+//        if ([value isKindOfClass:[NSString class]]) {
+//            if ([value isEqualToString:@"action.back"])
+//            {
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [weakself popAction];
+        });
+        
+//            }
+//        }
+    };
+    NSLog(@"value is %ld",result);
+}
+
+- (void)popAction
+{
+    
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)dealloc
+{
+    NSLog(@"webView is %@",testWebView);
 }
 
 @end
